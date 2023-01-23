f9692bdb9a70dd00a9bea331b2d4d70d249dbac1
Tusshed <103804885+Tusshed@users.noreply.github.com>
2023-01-24 02:09:32 +1100

    Property Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Fragment } from "react";
 import "./App.css";
 import Navbar from "./Component/Navbar";
 import Home from "./Component/Home";
+import Property from "./Component/Property";
 const App = () => {
   return (
     <Fragment>
       <Navbar />
       <Home />
+      <Property />
     </Fragment>
   );
 };
