8b2f022deedd7d39e8afeab1d03bff936e380ead
Tusshed <103804885+Tusshed@users.noreply.github.com>
2023-01-29 10:41:51 +1100

    Services Updated Used Aos animation,however services still need css brush up

--- src/Component/Services.js
+++ src/Component/Services.js
@@ -1,11 +1,17 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import ServicesDetail from "../Detail/ServicesDetail";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import "../Css/Services.css";
 
 const Services = () => {
   const [detail, setDetail] = useState(ServicesDetail);
+  useEffect(() => {
+    Aos.init({ duration: 2000, once: true, easing: "ease" });
+  }, []);
   return (
     <Fragment>
-      <section className="services">
+      <section data-aos="fade-up" className="services">
         <div className="heading">
           <span>My Services</span>
           <h2> Begin Your Journey To Becoming A Property Owner Today</h2>
@@ -23,7 +29,6 @@ const Services = () => {
                     <div className="services-content">
                       <h3>{detail.heading}</h3>
                       <p>{detail.info}</p>
-                      {/*Infrastructure,Expert,Locked Payment*/}
                     </div>
                   </div>
                 </div>
--- src/Detail/ServicesDetail.js
+++ src/Detail/ServicesDetail.js
@@ -7,21 +7,21 @@ const ServicesDetail = [
   },
   {
     id: "2",
-    Image: "ServicesImages/icons8-client-64.png",
+    Image: "ServicesImages/icons8-handshake-64.png",
     heading: "Infrastructure Negotiation",
-    info: "Our properties have the essential requirements for a contempoary family to live in,within affordable budget cost",
+    info: "Our properties have the essential requirements for a contempoary family to live in,with good road network, portable water, 24hrs light & round the clock security etc.within affordable budget cost",
   },
   {
     id: "3",
-    Image: "ServicesImages/icons8-agreement-80.png",
+    Image: "ServicesImages/icons8-innovation-80.png",
     heading: "Expert Consult",
-    info: "Each property boasts of excellent rental and resell value,which indicates an excellent return rate for the buyer. I will be providing you with quality information on our properties",
+    info: "Each property boasts of excellent rental and resell value,which indicates excellent returns of profits for the buyers as we are always finding new ways to market your property.If you are buying for investment purposes.",
   },
   {
     id: "4",
     Image: "ServicesImages/icons8-money-box-64.png",
     heading: "Locked Payment",
-    info: "Home sales packages are available for each State and can be inspected before purchase and all forms are sold with satisfaction guarantee.",
+    info: "We regularly outperform our competition and achieve affordable record prices for our local properties,our homes are very affordable, we have something for both the high & middle class earners with flexible payment plans.",
   },
 ];
 export default ServicesDetail;
